7febe70a3a5955ed28f3c03ddaea45c54962988d
Jacob Riofrio <111601406+JacobRiofrio@users.noreply.github.com>
2023-03-21 17:04:27 +0000

    Tue, Mar 21, 2023, 10:04 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,9 @@
 const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counterinnerText = '0'
+
+    const.updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+    }
+})
