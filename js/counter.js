f9e11dcbfb82f034360ea863bc64b4f2dbeb97a9
Jacob Riofrio <111601406+JacobRiofrio@users.noreply.github.com>
2023-03-21 17:08:45 +0000

    Tue, Mar 21, 2023, 10:08 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -4,6 +4,13 @@ counters.forEach(counter => {
     counterinnerText = '0'
 
     const.updateCounter = () => {
-        const target = counter.getAttribute('data-target')
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)} `
+        }
     }
 })
